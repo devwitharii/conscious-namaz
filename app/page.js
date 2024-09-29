575efa543e85d0418d44ca2b9ca86c0583034cba
phariwac <hari.p@webandcrafts.com>
2024-09-30 00:09:06 +0530

    homebanner added

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export const metadata = {
 export default function Home() {
   return (
     <>
-      {/* <HomeBanner /> */}
+      <HomeBanner />
       <SubscriptionPlan/>
       <HomeReviews/>
       <HomeInsights />
